3ea7fa6b4202a5d2dd526b47a56a53981c795367
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2024-10-08 22:08:07 +0100

    Prettier + eslint fix

--- gym-track/eslint.config.mjs
+++ gym-track/eslint.config.mjs
@@ -1,8 +1,8 @@
-import typescriptEslintParser from '@typescript-eslint/parser'
-import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin'
-import prettierConfig from 'eslint-config-prettier'
-import vuePlugin from 'eslint-plugin-vue'
-import vueEslintParser from 'vue-eslint-parser'
+import typescriptEslintParser from '@typescript-eslint/parser';
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import vuePlugin from 'eslint-plugin-vue';
+import prettierPlugin from 'eslint-plugin-prettier';
+import vueEslintParser from 'vue-eslint-parser';
 
 export default [
   {
@@ -17,6 +17,7 @@ export default [
     plugins: {
       '@typescript-eslint': typescriptEslintPlugin,
       vue: vuePlugin,
+      prettier: prettierPlugin,
     },
     rules: {
       curly: ['warn', 'all'],
@@ -28,6 +29,7 @@ export default [
       '@typescript-eslint/no-unused-vars': 'warn',
       'vue/script-setup-uses-vars': 'error',
       'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'prettier/prettier': 'warn',
     },
   },
-]
+];
