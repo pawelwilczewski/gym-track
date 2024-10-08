bc6ec9e21203ecb333c2957708d00db768ddc0ba
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2024-10-08 22:14:50 +0100

    Force static typing in ts

--- gym-track/src/App.vue
+++ gym-track/src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button';
 
-const test = () => {
-  alert('hello');
-};
+const test = (): void => alert('hello');
 </script>
 
 <template>
