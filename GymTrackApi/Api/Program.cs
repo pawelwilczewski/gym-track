e4bd53feaced1274f0b61f39b1408db892d161d1
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2024-10-11 18:06:19 +0100

    Using default authentication (using cbc for encryption)

--- GymTrackApi/Api/Program.cs
+++ GymTrackApi/Api/Program.cs
@@ -1,10 +1,8 @@
-using System.Text;
 using Api.Routes;
 using Domain.Models.User;
 using Infrastructure;
 using Infrastructure.Persistence;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,26 +14,7 @@ if (builder.Environment.IsDevelopment())
 	builder.Services.AddSwaggerGen();
 }
 
-builder.Services
-	.AddAuthentication(IdentityConstants.BearerScheme)
-	.AddJwtBearer(options =>
-	{
-		var jwtSettings = builder.Configuration.GetSection("Jwt");
-		var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]!);
-
-		options.TokenValidationParameters = new TokenValidationParameters
-		{
-			ValidateIssuer = true,
-			ValidateAudience = true,
-			ValidateLifetime = true,
-			ValidateIssuerSigningKey = true,
-			ValidIssuer = jwtSettings["Issuer"],
-			ValidAudience = jwtSettings["Audience"],
-			IssuerSigningKey = new SymmetricSecurityKey(key),
-			ClockSkew = TimeSpan.Zero
-		};
-	});
-
+builder.Services.AddAuthentication();
 builder.Services.AddAuthorization();
 
 builder.Services
