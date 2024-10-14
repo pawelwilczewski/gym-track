ac6ded760ff381ad9fd144572ff709b546c34094
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2024-10-14 01:28:36 +0100

    Update delete behaviour and fix step info relation migration

---
+++ GymTrackApi/Infrastructure/Persistence/Migrations/20241014002742_UpdateDeletesAndStepInfoRelation.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDeletesAndStepInfoRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserWorkouts_Workouts_WorkoutId",
+                schema: "Workout",
+                table: "UserWorkouts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkoutExercises_ExerciseInfos_ExerciseInfoId",
+                schema: "Workout",
+                table: "WorkoutExercises");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserWorkouts_Workouts_WorkoutId",
+                schema: "Workout",
+                table: "UserWorkouts",
+                column: "WorkoutId",
+                principalSchema: "Workout",
+                principalTable: "Workouts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkoutExercises_ExerciseInfos_ExerciseInfoId",
+                schema: "Workout",
+                table: "WorkoutExercises",
+                column: "ExerciseInfoId",
+                principalSchema: "Workout",
+                principalTable: "ExerciseInfos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserWorkouts_Workouts_WorkoutId",
+                schema: "Workout",
+                table: "UserWorkouts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkoutExercises_ExerciseInfos_ExerciseInfoId",
+                schema: "Workout",
+                table: "WorkoutExercises");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserWorkouts_Workouts_WorkoutId",
+                schema: "Workout",
+                table: "UserWorkouts",
+                column: "WorkoutId",
+                principalSchema: "Workout",
+                principalTable: "Workouts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkoutExercises_ExerciseInfos_ExerciseInfoId",
+                schema: "Workout",
+                table: "WorkoutExercises",
+                column: "ExerciseInfoId",
+                principalSchema: "Workout",
+                principalTable: "ExerciseInfos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
