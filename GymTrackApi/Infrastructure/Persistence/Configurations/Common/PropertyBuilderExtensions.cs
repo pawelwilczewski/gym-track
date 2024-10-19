56afa357e4dbf20db81a62723a51ee1c63459314
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2024-10-19 22:11:33 +0100

    Fix name/description configuration

--- GymTrackApi/Infrastructure/Persistence/Configurations/Common/PropertyBuilderExtensions.cs
+++ GymTrackApi/Infrastructure/Persistence/Configurations/Common/PropertyBuilderExtensions.cs
@@ -6,8 +6,12 @@ namespace Infrastructure.Persistence.Configurations.Common;
 internal static class PropertyBuilderExtensions
 {
 	public static PropertyBuilder<TProperty> ConfigureName<TProperty>(this PropertyBuilder<TProperty> builder) =>
-		builder.HasConversion(Name.Converter, Name.Comparer);
+		builder
+			.HasConversion(Name.Converter, Name.Comparer)
+			.HasMaxLength(Name.MAX_LENGTH);
 
 	public static PropertyBuilder<TProperty> ConfigureDescription<TProperty>(this PropertyBuilder<TProperty> builder) =>
-		builder.HasConversion(Description.Converter, Description.Comparer);
+		builder
+			.HasConversion(Description.Converter, Description.Comparer)
+			.HasMaxLength(Description.MAX_LENGTH);
 }
