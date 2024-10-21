3e66f414584700939e08ca653f1f0bced750aee3
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2024-10-21 08:45:52 +0100

    Fixed activator error

--- GymTrackApi/Domain/Validation/ValidatedText.cs
+++ GymTrackApi/Domain/Validation/ValidatedText.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Reflection;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
@@ -8,12 +9,22 @@ public abstract record class ValidatedText<T> where T : ValidatedText<T>
 {
 	public static ValueConverter<T, string> Converter { get; } = new(
 		text => text.Value,
-		value => (T)Activator.CreateInstance(typeof(T), value)!);
+		value => (T)Activator.CreateInstance(
+			typeof(T),
+			BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public,
+			null,
+			new object[] { value },
+			null)!);
 
 	public static ValueComparer<T> Comparer { get; } = new(
 		(a, b) => (a == null && b == null) || (a != null && b != null && a.Value == b.Value),
 		value => value.GetHashCode(),
-		text => (T)Activator.CreateInstance(typeof(T), text)!);
+		text => (T)Activator.CreateInstance(
+			typeof(T),
+			BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public,
+			null,
+			new object[] { text },
+			null)!);
 
 	private string Value { get; set; }
 	protected abstract TextValidator Validator { get; }
