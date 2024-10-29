064f5ec5348f7432b5cda03f2acb0dab101f12a1
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2024-10-29 05:30:55 +0000

    Added todos, wip user claim

--- GymTrackApi/Api.Tests/Mocks/HttpContextMocks.cs
+++ GymTrackApi/Api.Tests/Mocks/HttpContextMocks.cs
@@ -10,4 +10,15 @@ internal static class HttpContextMocks
 	{
 		User = new ClaimsPrincipal(new ClaimsIdentity([new Claim(ClaimTypes.Role, Role.ADMINISTRATOR)]))
 	};
+
+	public static HttpContext User { get; } = new DefaultHttpContext
+	{
+		User = new ClaimsPrincipal()
+	};
+
+	// TODO: ^ Fix this and \/
+	// TODO: Add basic admin and user entities to the "empty" db so they can be used easily
+
+	// TODO: more appropriate response types (i.e. 201 Created)
+	// TODO: finish writing tests
 }
