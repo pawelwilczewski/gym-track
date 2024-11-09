f06f5b1579efa09119ced076ec8fb644d1100b98
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2024-11-09 18:46:36 +0000

    Register & confirm user test implemented

--- GymTrackApi/Api.Tests.Functional/AuthenticationTests.cs
+++ GymTrackApi/Api.Tests.Functional/AuthenticationTests.cs
@@ -1,6 +1,13 @@
 using System.Net;
 using System.Net.Http.Json;
+using System.Text;
+using Application.Persistence;
+using Domain.Models.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Api.Tests.Functional;
 
@@ -10,14 +17,35 @@ internal sealed class AuthenticationTests
 	[ClassDataSource<FunctionalTestWebAppFactory>(Shared = SharedType.PerTestSession)]
 	public async Task RegisterAndLogin_ValidUser_ReturnsCorrectResponse(FunctionalTestWebAppFactory factory)
 	{
+		const string email = "user@user.com";
+		const string password = "User!123";
+
 		var httpClient = factory.CreateClient();
 		var response = await httpClient.PostAsJsonAsync("auth/register", new RegisterRequest
 			{
-				Email = "user@user.com",
-				Password = "User!123"
+				Email = email,
+				Password = password
 			})
 			.ConfigureAwait(false);
 
 		await Assert.That(response.StatusCode).IsEqualTo(HttpStatusCode.NoContent);
+
+		using var scope = factory.Services.CreateScope();
+
+		var dataContext = scope.ServiceProvider.GetRequiredService<IDataContext>();
+		var user = await dataContext.Users.FirstAsync(user => user.Email == email).ConfigureAwait(false);
+
+		var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+		var code = await userManager.GenerateEmailConfirmationTokenAsync(user).ConfigureAwait(false);
+		var encodedCode = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
+		var query = QueryHelpers.AddQueryString("auth/confirmEmail", new Dictionary<string, string?>
+		{
+			{ "userId", user.Id.ToString() },
+			{ "code", encodedCode }
+		});
+
+		response = await httpClient.GetAsync(query).ConfigureAwait(false);
+		await Assert.That(response.StatusCode).IsEqualTo(HttpStatusCode.OK);
 	}
 }
