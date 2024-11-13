18c5e9778207320373bc310fdbfc8d8908969b2b
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2024-11-13 23:40:18 +0000

    Initial log in form setup

--- gym-track/src/pages/auth/LogIn.vue
+++ gym-track/src/pages/auth/LogIn.vue
@@ -1,7 +1,30 @@
 <script setup lang="ts">
 import DefaultLayout from '@/components/layouts/DefaultLayout.vue';
+import Button from '@/components/ui/button/Button.vue';
+import Input from '@/components/ui/input/Input.vue';
+import Label from '@/components/ui/label/Label.vue';
 </script>
 
 <template>
-  <DefaultLayout>Hello</DefaultLayout>
+  <DefaultLayout>
+    <div class="mx-8">
+      <form
+        class="mx-auto border border-border rounded-xl max-w-sm flex flex-col gap-4 p-4"
+      >
+        <div>
+          <Label for="username">Username</Label>
+          <Input id="username" type="email" />
+        </div>
+        <div>
+          <Label for="password">Password</Label>
+          <Input id="password" type="password" />
+        </div>
+        <div class="flex justify-between flex-wrap gap-2">
+          <a href="/forgotPassword" class="text-sm">Forgot Password?</a>
+          <a href="/signUp" class="text-sm">Sign Up</a>
+        </div>
+        <Button class="mx-auto px-8" type="submit">Log In</Button>
+      </form>
+    </div>
+  </DefaultLayout>
 </template>
