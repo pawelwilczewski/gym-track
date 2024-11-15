b1e085b161ac933add69a8e3be91acf48d83a0c2
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2024-11-15 04:35:31 +0000

    Moved file

--- gym-track/src/main.ts
+++ gym-track/src/main.ts
@@ -3,5 +3,6 @@ import App from './App.vue';
 import router from './router';
 import './assets/index.css';
 import '@/scripts/Math/RoundingExtensions';
+import '@/scripts/Styling/AutoUpdateBodyDarkClass';
 
 createApp(App).use(router).mount('#app');
