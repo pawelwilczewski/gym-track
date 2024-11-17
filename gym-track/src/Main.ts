8bb3ac91e1a54e8e981d2bb5d4842d41a2e67d0f
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2024-11-17 03:58:28 +0000

    Cleanup scope of auto dark script

--- gym-track/src/Main.ts
+++ gym-track/src/Main.ts
@@ -3,6 +3,5 @@ import App from './App.vue';
 import router from './Router';
 import './assets/index.css';
 import '@/scripts/math/RoundingExtensions';
-import '@/scripts/styling/AutoUpdateBodyDarkClass';
 
 createApp(App).use(router).mount('#app');
