732b2b87d0a431ff6270bb226d1c4d6d0147a0c9
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2024-11-25 03:41:25 +0000

    Delete workout button

--- gym-track/src/components/app/workout/Workout.vue
+++ gym-track/src/components/app/workout/Workout.vue
@@ -1,21 +1,42 @@
 <script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue';
+import { ErrorHandler } from '@/scripts/errors/ErrorHandler';
+import { toastErrorHandler } from '@/scripts/errors/Handlers';
+import { apiClient } from '@/scripts/http/Clients';
 import { GetWorkoutResponse } from '@/scripts/schema/Types';
+import { Ref, ref } from 'vue';
+import WorkoutExercises from './WorkoutExercises.vue';
 
-defineProps<{
+const props = defineProps<{
   workout: GetWorkoutResponse;
 }>();
+
+async function deleteWorkout(): Promise<void> {
+  if (!workout) {
+    return;
+  }
+  const response = await apiClient.delete(
+    `/api/v1/workouts/${props.workout.id}`
+  );
+  if (
+    ErrorHandler.forResponse(response).handleFully(toastErrorHandler).wasError()
+  ) {
+    return;
+  }
+  workout.value = undefined;
+}
+
+const workout: Ref<GetWorkoutResponse | undefined> = ref(props.workout);
 </script>
 
 <template>
   <div
+    v-if="workout"
     class="mx-auto border border-border rounded-xl w-80 flex flex-col gap-6 p-8"
   >
     <h3>{{ workout.name }}</h3>
-    <ol>
-      <li v-for="exercise in workout.exercises">
-        {{ exercise.index }}
-        {{ exercise.workoutId }}
-      </li>
-    </ol>
+    <Button @click="deleteWorkout">Delete</Button>
+    <h4>Exercises</h4>
+    <WorkoutExercises :workout="workout" />
   </div>
 </template>
