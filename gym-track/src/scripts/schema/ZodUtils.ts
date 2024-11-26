516ca3b32604e63fa4ff433257bd43079ff50245
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2024-11-26 21:19:49 +0000

    Enum flags logic cleaner and documented

--- gym-track/src/scripts/schema/ZodUtils.ts
+++ gym-track/src/scripts/schema/ZodUtils.ts
@@ -1,21 +1,58 @@
 import { z, ZodType } from 'zod';
 
-export type EnumFlags = Record<string, string | number>;
-export type EnumFlagsNumber = EnumFlags[keyof EnumFlags] & number;
+type EnumFlags = Record<string, string | number>;
+type EnumFlagsNumber = EnumFlags[keyof EnumFlags] & number;
 
-export function zodEnumFlagsAsNumber(enumObj: EnumFlags): ZodType {
-  const allValidBits = getAllValidEnumBits(enumObj);
+/**
+ * Accepts an int enum flag value and checks if it's
+ * valid for the given enum type.
+ */
+export function zodEnumFlagsAsNumber(
+  enumType: EnumFlags,
+  isRequired: boolean = true
+): ZodType {
+  const allValidBits = getAllValidEnumBits(enumType);
   return z
     .number()
     .int()
     .refine(
       (value): value is EnumFlagsNumber => (value & ~allValidBits) === 0,
       { message: 'Invalid flags value.' }
-    );
+    )
+    .refine(value => isRequired && value > 0, { message: 'Required' });
 }
 
-export function getAllValidEnumBits(enumObj: EnumFlags): number {
-  return Object.values(enumObj)
+/**
+ * Accepts enum flag string array input, i.e. `['1', '2', '8']`, checks if they
+ * are valid enum flags values for the given enum type, and returns the sum of
+ * all given flags, i.e. `['1', '2', '8'] -> 11`, so it can be worked with like
+ * an enum flag value.
+ *
+ * This can be used with toggle groups.
+ */
+export function zodEnumFlagsAsStringArray(
+  enumType: EnumFlags,
+  isRequired: boolean = true
+): ZodType {
+  const allValidBits = getAllValidEnumBits(enumType);
+  return z
+    .array(z.string())
+    .refine(
+      value =>
+        value.every(str => {
+          const num = parseInt(str, 10);
+          return !isNaN(num) && (num & ~allValidBits) === 0;
+        }),
+      { message: 'Invalid flags in string array.' }
+    )
+    .transform(value => {
+      return calculateBitSumFromStringArray(value, allValidBits);
+    })
+    .refine(value => isRequired && value > 0, { message: 'Required' });
+}
+
+export function getAllValidEnumBits(enumType: EnumFlags): number {
+  return Object.values(enumType)
     .filter((value): value is EnumFlagsNumber => typeof value === 'number')
     .reduce((acc, value) => acc | value, 0);
 }
@@ -35,22 +72,4 @@ function calculateBitSumFromStringArray(
       return sum + num;
     }, 0);
   return bitSum;
-}
-
-export function zodEnumFlagsAsStringArray(enumObj: EnumFlags): ZodType {
-  const allValidBits = getAllValidEnumBits(enumObj);
-  return z
-    .array(z.string())
-    .refine(
-      value =>
-        value.every(str => {
-          const num = parseInt(str, 10);
-          return !isNaN(num) && (num & ~allValidBits) === 0;
-        }),
-      { message: 'Invalid flags in string array.' }
-    )
-    .transform(value => {
-      return calculateBitSumFromStringArray(value, allValidBits);
-    })
-    .refine(value => value > 0, { message: 'Required' });
 }
