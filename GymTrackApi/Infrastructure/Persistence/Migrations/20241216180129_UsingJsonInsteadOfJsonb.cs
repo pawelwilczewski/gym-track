0cb929ab660b91cafc4f71868fe8c890647b8981
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2024-12-16 18:10:13 +0000

    Using json to prevent reordering properties

---
+++ GymTrackApi/Infrastructure/Persistence/Migrations/20241216180129_UsingJsonInsteadOfJsonb.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsingJsonInsteadOfJsonb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Metric",
+                schema: "Workout",
+                table: "WorkoutExerciseSets",
+                type: "json",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "jsonb");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Metric",
+                schema: "Workout",
+                table: "WorkoutExerciseSets",
+                type: "jsonb",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "json");
+        }
+    }
+}
