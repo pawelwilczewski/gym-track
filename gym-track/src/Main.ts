bbc80da39fc44a10d3366a3c4ac5694267ab1bfe
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2024-12-29 00:01:06 +0000

    Added pinia

--- gym-track/src/Main.ts
+++ gym-track/src/Main.ts
@@ -2,5 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './Router';
 import './assets/index.css';
+import { createPinia } from 'pinia';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+
+app.use(router);
+app.use(createPinia());
+
+app.mount('#app');
