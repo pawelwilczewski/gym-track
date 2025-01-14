50d5eee90776a2b71cba130ba4fe4d74d4a8340d
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2025-01-14 22:43:17 +0000

    Added home page, removed dashboard link temporarily

--- gym-track/src/features/shared/components/header-content.vue
+++ gym-track/src/features/shared/components/header-content.vue
@@ -2,6 +2,7 @@
 import { Button } from '@/features/shared/components/ui/button';
 import TabButton from '@/features/shared/components/tab-button.vue';
 import { useAuth } from '@/features/auth/stores/use-auth';
+import { Dumbbell } from 'lucide-vue-next';
 
 const auth = useAuth();
 </script>
@@ -9,11 +10,13 @@ const auth = useAuth();
 <template>
   <nav class="flex justify-between gap-24">
     <h1 class="pb-3 px-3">
-      <RouterLink to="/" class="font-bold">Gym Track</RouterLink>
+      <RouterLink to="/" class="font-bold">
+        <Dumbbell class="w-12 h-12" />
+      </RouterLink>
     </h1>
     <div class="flex gap-6 flex-wrap">
       <div v-if="auth.isLoggedIn" class="flex gap-2">
-        <TabButton path="/dashboard" name="Dashboard"></TabButton>
+        <!-- <TabButton path="/dashboard" name="Dashboard"></TabButton> -->
         <TabButton path="/workouts" name="Workouts"></TabButton>
         <TabButton path="/exercises" name="Exercises"></TabButton>
       </div>
--- gym-track/src/features/shared/views/Home.vue
+++ gym-track/src/features/shared/views/Home.vue
@@ -1,12 +1,53 @@
 <script setup lang="ts">
-import DefaultLayout from '@/features/shared/layouts/default-layout.vue';
 import { Button } from '@/features/shared/components/ui/button';
-
-const test = (): void => alert('hello');
+import OneColumnLayout from '@/features/shared/layouts/one-column-layout.vue';
+import { ChartLine, File, PersonStanding } from 'lucide-vue-next';
 </script>
 
 <template>
-  <DefaultLayout>
-    <Button @click="test">Click!</Button>
-  </DefaultLayout>
+  <OneColumnLayout>
+    <div class="flex flex-col items-center px-4 py-10 space-y-16 text-center">
+      <div>
+        <h1 class="text-4xl font-bold sm:text-5xl mb-8">Manage Your Workout</h1>
+        <p class="text-lg max-w-2xl">
+          With
+          <span class="font-bold italic">Gym Track</span>
+          you can easily create, use, and edit your workout routines. Take your
+          fitness journey to the next level with personalized plans.
+        </p>
+      </div>
+
+      <div
+        class="flex flex-col sm:flex-row sm:space-x-4 space-y-4 sm:space-y-0"
+      >
+        <RouterLink to="/login">
+          <Button size="lg">Get Started</Button>
+        </RouterLink>
+      </div>
+
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-8 w-full max-w-4xl">
+        <div class="flex flex-col items-center p-6 rounded-lg shadow-md">
+          <File class="w-16 h-16 mb-4" />
+          <h3 class="text-xl font-semibold">Perfect your Routine</h3>
+          <p class="text-center">
+            Create personalized workout routines tailored to your goals.
+          </p>
+        </div>
+        <div class="flex flex-col items-center p-6 rounded-lg shadow-md">
+          <ChartLine class="w-16 h-16 mb-4" />
+          <h3 class="text-xl font-semibold">Track Progress</h3>
+          <p class="text-center">
+            Monitor your progress and stay motivated every step of the way.
+          </p>
+        </div>
+        <div class="flex flex-col items-center p-6 rounded-lg shadow-md">
+          <PersonStanding class="w-16 h-16 mb-4" />
+          <h3 class="text-xl font-semibold">Join Community</h3>
+          <p class="text-center">
+            Connect with fitness enthusiasts and share your journey.
+          </p>
+        </div>
+      </div>
+    </div>
+  </OneColumnLayout>
 </template>
