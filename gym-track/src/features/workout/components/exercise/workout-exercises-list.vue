88dbc87bd432e67c7c1a44de7fb3bfdcf04fc9c3
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2025-01-14 02:16:17 +0000

    Moved sorting to composable

--- gym-track/src/features/workout/components/exercise/workout-exercises-list.vue
+++ gym-track/src/features/workout/components/exercise/workout-exercises-list.vue
@@ -1,50 +1,21 @@
 <script setup lang="ts">
-import {
-  hashWorkoutExerciseKey,
-  WorkoutExerciseKey,
-} from '@/features/workout/types/workout-types';
 import WorkoutExercise from '@/features/workout/components/exercise/workout-exercise.vue';
 import { UUID } from 'node:crypto';
 import { useWorkoutExerciseKeys } from '@/features/workout/composables/use-workout-exercise-keys';
 import { useWorkout } from '@/features/workout/composables/use-workout';
-import { useWorkoutExercises } from '@/features/workout/stores/use-workout-exercises';
-import { ref, watch } from 'vue';
 
 const { workoutId } = defineProps<{
   workoutId: UUID;
 }>();
 
 const { workout } = useWorkout(workoutId);
-const exerciseKeys = useWorkoutExerciseKeys(workout);
-const exercises = useWorkoutExercises();
-
-const sortedExerciseKeys = ref<WorkoutExerciseKey[]>([]);
-
-watch(exercises.all, async () => {
-  if (
-    exerciseKeys.value.some(
-      key => exercises.all[hashWorkoutExerciseKey(key)] == undefined
-    )
-  ) {
-    return;
-  }
-
-  sortedExerciseKeys.value = [...exerciseKeys.value].sort(
-    (a, b) =>
-      exercises.all[hashWorkoutExerciseKey(a)].displayOrder -
-      exercises.all[hashWorkoutExerciseKey(b)].displayOrder
-  );
-});
-
-watch(exerciseKeys, () => exercises.fetchMultiple(exerciseKeys.value), {
-  immediate: true,
-});
+const { sortedByDisplayOrder: exerciseKeys } = useWorkoutExerciseKeys(workout);
 </script>
 
 <template>
   <ol class="list-decimal flex flex-col gap-4">
     <WorkoutExercise
-      v-for="key in sortedExerciseKeys"
+      v-for="key in exerciseKeys"
       :key="key.exerciseIndex"
       :exercise-key="key"
     />
--- gym-track/src/features/workout/composables/use-workout-exercise-keys.ts
+++ gym-track/src/features/workout/composables/use-workout-exercise-keys.ts
@@ -1,11 +1,42 @@
+import { useWorkoutExercises } from '@/features/workout/stores/use-workout-exercises';
 import {
   GetWorkoutResponse,
+  hashWorkoutExerciseKey,
   WorkoutExerciseKey,
 } from '@/features/workout/types/workout-types';
-import { computed, Ref } from 'vue';
+import { computed, ref, Ref, watch } from 'vue';
 
 export function useWorkoutExerciseKeys(
   workout: Ref<GetWorkoutResponse | undefined>
-): Ref<WorkoutExerciseKey[]> {
-  return computed(() => workout.value?.exercises ?? []);
+): {
+  exerciseKeys: Ref<WorkoutExerciseKey[]>;
+  sortedByDisplayOrder: Ref<WorkoutExerciseKey[]>;
+} {
+  const exerciseKeys = computed(() => workout.value?.exercises ?? []);
+
+  const exercises = useWorkoutExercises();
+
+  const sortedByDisplayOrder = ref<WorkoutExerciseKey[]>([]);
+
+  watch(exercises.all, async () => {
+    if (
+      exerciseKeys.value.some(
+        key => exercises.all[hashWorkoutExerciseKey(key)] == undefined
+      )
+    ) {
+      return;
+    }
+
+    sortedByDisplayOrder.value = [...exerciseKeys.value].sort(
+      (a, b) =>
+        exercises.all[hashWorkoutExerciseKey(a)].displayOrder -
+        exercises.all[hashWorkoutExerciseKey(b)].displayOrder
+    );
+  });
+
+  watch(exerciseKeys, () => exercises.fetchMultiple(exerciseKeys.value), {
+    immediate: true,
+  });
+
+  return { exerciseKeys, sortedByDisplayOrder };
 }
