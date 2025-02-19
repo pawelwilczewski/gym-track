4eb4a9659ff52dd4af5709c38ece1444ee4435ca
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2025-02-19 16:04:54 +0000

    32 bytes long refresh tokens (+ fixed length reference bug)

--- GymTrackApi/Domain/Common/ValueObjects/RefreshToken.cs
+++ GymTrackApi/Domain/Common/ValueObjects/RefreshToken.cs
@@ -5,7 +5,7 @@ namespace Domain.Common.ValueObjects;
 [ValueObject<string>]
 public readonly partial struct RefreshToken
 {
-	public const int BYTES_LENGTH = 16;
+	public const int BYTES_LENGTH = 32;
 
 	private static Validation Validate(string input) =>
 		input.Length >= BYTES_LENGTH
---
+++ GymTrackApi/Infrastructure/Persistence/Migrations/20250219160420_LongerRefreshTokens.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class LongerRefreshTokens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RefreshToken",
+                schema: "Authentication",
+                table: "UserRefreshTokens",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(32)",
+                oldMaxLength: 32);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RefreshToken",
+                schema: "Authentication",
+                table: "UserRefreshTokens",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(64)",
+                oldMaxLength: 64);
+        }
+    }
+}
